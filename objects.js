8c5c7f7cc7a11e043d85197a28517086cbf9e893
mynorgc79 <mynorguarchaj99@gmail.com>
2024-03-14 11:04:13 -0600

    feat: C-JS, capitulo 5 del curso de js

---
+++ objects.js
@@ -0,0 +1,35 @@
+class Persona{
+    constructor(nombre, apellido, altura){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.altura = altura;
+    }
+
+    saludar(){
+        console.log(`Hola, mi nombre es ${this.nombre} ${this.apellido}`);
+    }
+
+    soyAlto(){
+        return this.altura > 1.8;
+    }
+}
+
+class Desarrollador extends Persona{
+    constructor(nombre, apellido, altura){
+        super(nombre, apellido, altura);
+    }
+
+    saludar(){
+        console.log(`Hola, mi nombre es ${this.nombre} ${this.apellido} y soy desarrollador`);
+    }
+}
+
+
+
+
+
+
+
+
+
+var mynor = new persona();
